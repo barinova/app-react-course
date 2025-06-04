e231b6595f0e75af12035e48eb4d0944492686c3
Anastasiia Barinova <anastasiia.barinova@benify.com>
2025-06-04 15:56:49 +0200

    extendable text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,20 @@
 import './App.css';
-import { useState } from 'react';
-import { Button } from './components/Button/Button.tsx';
+import { ExpandableText } from './components/ExpandableText.tsx';
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: 'John',
-    },
-  });
-
-  const [pizza, setPizza] = useState({
-    name: 'Margherita',
-    toppings: ['cheese', 'tomato'],
-  });
-
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      {
-        id: 1,
-        title: 'Pizza Margherita',
-        price: 10,
-        quantity: 1,
-      },
-      {
-        id: 2,
-        title: 'Pizza Pepperoni',
-        price: 12,
-        quantity: 12,
-      },
-    ],
-  });
-
-  const changeQuantity = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map(item => ({
-        ...item,
-        quantity: item.quantity + 1,
-      })),
-    });
-  };
-
-  const addTopping = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, 'basil'] });
-  };
-
-  const handleClick = () => {
-    setGame({
-      ...game,
-      player: {
-        name: 'Jane',
-      },
-    });
-  };
-
   return (
     <div>
-      <section>
-        <p>{game.player.name}</p>
-        <Button onClick={handleClick}>Change name</Button>
-      </section>
-
-      <section>
-        <p>{pizza.toppings.join(', ')}</p>
-        <Button onClick={addTopping}>Add topping</Button>
-      </section>
-
-      <section>
-        {cart.items.map(item => (
-          <p>{item.quantity}</p>
-        ))}
-        <Button onClick={changeQuantity}>Change quantity</Button>
-      </section>
+      <ExpandableText maxCharacters={15}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </ExpandableText>
     </div>
   );
 }
