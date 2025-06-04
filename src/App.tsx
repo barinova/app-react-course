1403861e55325e1fdec829257a8802c8830871b3
Anastasiia Barinova <anastasiia.barinova@benify.com>
2025-06-04 14:55:06 +0200

    use immer to make state mutable

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ListGroup from './components/ListGroup.tsx';
 import { Button } from './components/Button/Button.tsx';
 import Alert from './components/Alert.tsx';
 import { useState } from 'react';
+import { produce } from 'immer';
 
 function App() {
   const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
@@ -11,6 +12,30 @@ function App() {
   };
 
   const [alertVisible, setAlertVisibility] = useState(false);
+  const [bugs, setBugs] = useState([
+    {
+      name: 'bug',
+      id: 3,
+      fixed: false,
+    },
+    {
+      name: 'bug1',
+      id: 18,
+      fixed: false,
+    },
+  ]);
+
+  const handleClick = () => {
+    // setBugs(bugs.map(bug => (bug.id === 3 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce(draft => {
+        const bug = draft.find(bug => bug.id === 3);
+        if (bug) {
+          bug.fixed = true;
+        }
+      }),
+    );
+  };
 
   return (
     <div>
@@ -24,6 +49,15 @@ function App() {
       ></ListGroup>
       <div className="mt-4">
         <Button onClick={() => setAlertVisibility(true)}>Primary Button</Button>
+      </div>
+
+      <div className="mt-4">
+        {bugs.map(bug => (
+          <p>
+            {bug.id} {bug.fixed ? 'fixed' : 'not fixed'}
+          </p>
+        ))}
+        <Button onClick={() => handleClick()}>Click</Button>
       </div>
     </div>
   );
