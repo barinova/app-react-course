dfabe84c204898e470b0631f47750fda6e913a59
Anastasiia Barinova <anastasiia.barinova@benify.com>
2025-06-06 16:27:03 +0200

    http request with loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
 import './App.css';
-import { Form } from './components/Form.tsx';
+import { useEffect, useState } from 'react';
+import axios, { CanceledError } from 'axios';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const abortController = new AbortController();
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: abortController.signal,
+      })
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(er => {
+        if (er instanceof CanceledError) {
+          return;
+        }
+        setError(er);
+      })
+      .finally(() => {
+        console.log('Request completed');
+        setIsLoading(false);
+      });
+
+    return () => abortController.abort();
+  }, []);
+
   return (
-    <div>
-      <Form></Form>
-    </div>
+    <>
+      {isLoading && <div className="spinner-border"></div>}
+      {error ? (
+        <span className="error">Error occurred</span>
+      ) : (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              {user.name} (ID: {user.id})
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
